9df4a4f650d0e0b7449862b493d298140b7a04a5
Shubham V. <shubhamd.vispute@gmail.com>
2025-05-04 01:21:52 +0530

    Assignment 1

--- src/language/basics/Assignment_1.java
+++ src/language/basics/Assignment_1.java
@@ -1,10 +1,64 @@
 package language.basics;
 
-public class Assignment_1 {
+public class Assignment_1 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		//Temperature of City in Degree Celsius
+		float temp=25.5f;
+		
+		//Whether a customer has place an Order
+		boolean status=false;
+		
+		//persons phone number
+		long phoneNumber=8830305561L;
+		
+		//Amount of Money in customers bank account
+		float balance=1000.50f;
+		
+		//persons email address
+		String EMail="shubhamd.vispute@gmail.com";
+		
+		//coordinates of locations
+		float coordinates=37.7749f-122.4194f;
+		
+		//person's maritial status
+		boolean M_Status=true;
+		
+		//Current Year
+		short year=2025;
+		
+		//Number of followers on social media platform
+		int followers=1000000;
+		
+		//Rating of a movie
+		float rating=7.5f;
+		
+		//persons blood type
+		char Blood_Type='A';
+		
+		//Title of the Book
+		String title="To kill a Mocking Bird";
+		
+		//Number of Employee in a Company
+		short numberOfEmployee=500;
+		
+		//Time of an Event
+		String time="2:30 PM";
+		
+		//Name if Country
+		String nameOfCountry="India";
+		
+		//person's Eye Color
+		String eyeColour="Brown";
+		
+		//person's Birth place
+		String birthPlace="New York City";
+		
+		//Distance between two cities
+		
+		float distance=200.5f;
 	}
 
 }
